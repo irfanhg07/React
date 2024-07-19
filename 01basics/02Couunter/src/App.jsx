51fe63b0058a40118f4a28a953ea83cd896e44f8
irfan.hussain <irfan.hussain@nineleaps.com>
2024-07-19 15:36:01 +0530

    Added the hooks code

---
+++ 01basics/02Couunter/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  const addValue = ()=>{
+  if(counter >= 20){
+    <h3>You are exceeding the counter</h3>
+    setCounter(20);
+  }
+  else{
+
+
+    console.log("Clicked ", counter);
+    setCounter(counter+1)
+  }
+  }
+
+  const subValue = ()=>{
+    if(counter <= 0){
+      <h3>You are exceeding the counter</h3>
+      setCounter(0);
+    }
+    else
+    setCounter(counter -1);
+  }  
+  return (
+    <>
+      <h1>This is vite app </h1>
+      <h2>Counter {counter}</h2>
+
+      <button
+      onClick={addValue}>Increment {counter}</button>
+
+      <button 
+      onClick={subValue}>Decrement {counter}</button>
+    </>
+  )
+}
+
+export default App
